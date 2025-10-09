a2d2b99dbc42ea5205524cd4c318dee4c31b7458
Rupak-Biswas04 <biswasrupak333@gmail.com>
2025-10-09 13:20:22 +0530

    Added links for navigating between Home and About and changing title dynamically

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,6 @@
+import { Link, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+
 function Navbar({mode, toggleMode}) {
     const bgColours = {
         light: "Light Mode",
@@ -5,47 +8,51 @@ function Navbar({mode, toggleMode}) {
         primary: "Blue Mode",
         success: "Green Mode",
         danger: "Red Mode"
-    }
+    };
+
+    const location = useLocation();
+    useEffect(() => {
+        document.title = 
+        location.pathname === "/about" ?
+        "TextUtitlity - About" : "TextUtitlity - Home" 
+    }, [location]);
+
     return (
         <nav className={`navbar navbar-expand-lg navbar-${(mode === "dark" || mode === "primary" || mode === "success" || mode === "danger")?"dark":"light"} bg-${mode}`}>
-        <div className="container-fluid">
-            <a className="navbar-brand" href="#">TextUtility</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-            </button>
-            
-            <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-                <li className="nav-item"><a className="nav-link active" aria-current="page" href="#">Home</a></li>
-                <li className="nav-item"><a className="nav-link" href="#">About Us</a></li>
-            </ul>
-
-            {/* Dropdown */}
-            <div className="dropdown btn-grp" 
-            style={{border: (mode === "dark")? "1px solid grey" : "1px solid black"}}>
-                <button type="button" className={`btn btn-${mode}`} style={{width: "120px"}}>{bgColours[mode]}</button>
-                <button type="button" className={`btn btn-${mode} dropdown-toggle dropdown-toggle-split`} data-bs-toggle="dropdown" aria-expanded="false">
-                <span className="visually-hidden">Toggle Dropdown</span>
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/">TextUtility</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                <li><button className="btn btn-light" onClick={() => toggleMode("light")}>Light Mode</button></li>
-                <li><button className="btn btn-dark text-light" onClick={() => toggleMode("dark")}>Dark Mode</button></li>
-                <li><button className="btn btn-primary text-light" onClick={() => toggleMode("primary")}>Blue</button></li>
-                <li><button className="btn btn-success text-light" onClick={() => toggleMode("success")}>Green</button></li>
-                <li><button className="btn btn-danger text-light" onClick={() => toggleMode("danger")}>Red</button></li>
-                </ul>
-            </div>
+                
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <Link className={`nav-link ${location.pathname === "/" ? "active" : ""}`} to="/">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className={`nav-link ${location.pathname === "/about" ? "active" : ""}`} to="/about">About Us</Link>
+                        </li>
+                    </ul>
 
-            {/* Toggle Dark Mode */}
-            {/* <div className="form-check form-switch ms-3">
-                <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" onClick={toggleMode} />
-                <label htmlFor="flexSwitchCheckDefault">
-                {mode === "light" ? "Enable Dark Mode" : "Enable Light Mode"}
-                </label>
-            </div> */}
+                    {/* Dropdown */}
+                    <div className="dropdown btn-grp ms-3" 
+                    style={{border: (mode === "dark")? "1px solid grey" : "1px solid black", borderRadius: "8px"}}>
+                        <button type="button" className={`btn btn-${mode}`} style={{width: "120px"}}>{bgColours[mode]}</button>
+                        <button type="button" className={`btn btn-${mode} dropdown-toggle dropdown-toggle-split`} data-bs-toggle="dropdown" aria-expanded="false">
+                        <span className="visually-hidden">Toggle Dropdown</span>
+                        </button>
+                        <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <li><button className="btn btn-light" onClick={() => toggleMode("light")}>Light Mode</button></li>
+                        <li><button className="btn btn-dark text-light" onClick={() => toggleMode("dark")}>Dark Mode</button></li>
+                        <li><button className="btn btn-primary text-light" onClick={() => toggleMode("primary")}>Blue</button></li>
+                        <li><button className="btn btn-success text-light" onClick={() => toggleMode("success")}>Green</button></li>
+                        <li><button className="btn btn-danger text-light" onClick={() => toggleMode("danger")}>Red</button></li>
+                        </ul>
+                    </div>
+                </div>
             </div>
-        </div>
         </nav>
     );
 }
