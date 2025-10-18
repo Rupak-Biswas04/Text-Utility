5d430e1233d13b80d8bc5b6fbfac130e52896f7e
Rupak-Biswas04 <biswasrupak333@gmail.com>
2025-10-18 23:50:49 +0530

    Updated styling for footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,11 @@
 export default function Footer({mode}) {
+    const footerStyle = {
+        color: (mode === "light") ? "black" : "white",
+        backgroundColor: (mode == "dark") ? "rgba(33, 37, 41, 0.2)" : "rgba(255, 255, 255, 0.1)",
+        position: "relative"
+    }
     return(
-        <footer className={`footer bg-${mode} text-center py-4 mt-5`} 
-        style={{color: (mode === "light") ? "black" : "white"}}>
+        <footer className={`footer text-center py-4 mt-5`} style={footerStyle}>
             <span>&copy; Copyright TextUtility {new Date().getFullYear()}&nbsp;&nbsp;&nbsp;&nbsp;</span>
             <span>All rights reserved</span>
         </footer>
