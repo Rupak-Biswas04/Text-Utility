b7d0da9e30076545c86034a20af21bf883ccf33a
Rupak-Biswas04 <biswasrupak333@gmail.com>
2025-10-18 23:52:27 +0530

    Added background image and updated styling for better experience

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -3,21 +3,22 @@ import { useState } from "react"
 export default function Content({mode, showAlert}) {
     const [text, setText] = useState("")
     const myStyle = {
-        backgroundColor: (mode === "dark")? "#212529" : "transparent",
-        color: (mode === "dark" || mode === "primary") ? "white" : "black",
+        backgroundColor: (mode === "dark")? "rgba(33, 37, 41, 0.2)" : "rgba(255, 255, 255, 0.1)",
+        color: "white",
+        backdropFilter: "blur(6px)",
         border: (mode === "light")? "1px solid grey": "1px solid white",
     }
     function buttonStyle(isDisabled, isClearButton) {
         if(isDisabled){
             return {
-                boxShadow: (mode === "primary") ? "0 0 10px blue" : "0 0 10px grey",
-                opacity: 0.7,
+                boxShadow: "0 0 10px black",
+                opacity: 0.9,
                 cursor: "not-allowed"
             }
         }
         else {
             return {
-                boxShadow: (isClearButton) ? "0 0 10px 1px red" : (mode === "dark") ? "0 0 10px whitesmoke" : "0 0 10px blue"
+                boxShadow: (isClearButton) ? "0 0 10px 1px red" : "0 0 10px blue"
             }
         }
     }
@@ -69,21 +70,21 @@ export default function Content({mode, showAlert}) {
         <>
             <div className="container">
                 <div className="my-4">
-                    <h3 className="mb-4">Enter text below to manipulate</h3>
-                    <textarea className="form-control" value={text} onChange={handleText} placeholder="Enter text here" rows="6" style={myStyle}/>
+                    <h3 className="mb-4">Enter text below to analyze and manipulate</h3>
+                    <textarea className="form-control" value={text} onChange={handleText} placeholder="Enter text here" rows="10" style={myStyle}/>
 
-                    <button className="btn btn-primary px-4 my-3 me-5" disabled={text === ""} onClick={handleUpperCase} style={buttonStyle(text === "", false)}>UPPER CASE</button>
-                    <button className="btn btn-primary px-4 my-3 me-5" disabled={text === ""} onClick={handleLowerCase} style={buttonStyle(text === "", false)}>lower case</button>
-                    <button className="btn btn-primary px-4 my-3 me-5" disabled={text === ""} onClick={handleTitleCase} style={buttonStyle(text === "", false)}>Title Case</button>
-                    <button className="btn btn-primary px-4 my-3 me-5" disabled={text === ""} onClick={handleSentenceCase} style={buttonStyle(text === "", false)}>Sentence case</button>
-                    <button className="btn btn-primary px-4 my-3 me-5" disabled={text === ""} onClick={handleExtraSpace} style={buttonStyle(text === "", false)}>Remove Extra Space</button>
-                    <button className="btn btn-danger px-4 my-3 me-5" disabled={text === ""} onClick={handleClear} style={buttonStyle(text === "", true)}>Clear Text</button>
+                    <button className="btn btn-primary px-4 my-3 me-3 me-sm-4" disabled={text === ""} onClick={handleUpperCase} style={buttonStyle(text === "", false)}>UPPER CASE</button>
+                    <button className="btn btn-primary px-4 my-3 me-3 me-sm-4" disabled={text === ""} onClick={handleLowerCase} style={buttonStyle(text === "", false)}>lower case</button>
+                    <button className="btn btn-primary px-4 my-3 me-3 me-sm-4" disabled={text === ""} onClick={handleTitleCase} style={buttonStyle(text === "", false)}>Title Case</button>
+                    <button className="btn btn-primary px-4 my-3 me-3 me-sm-4" disabled={text === ""} onClick={handleSentenceCase} style={buttonStyle(text === "", false)}>Sentence case</button>
+                    <button className="btn btn-primary px-4 my-3 me-3 me-sm-4" disabled={text === ""} onClick={handleExtraSpace} style={buttonStyle(text === "", false)}>Remove Extra Space</button>
+                    <button className="btn btn-danger px-4 my-3 me-3 me-sm-4" disabled={text === ""} onClick={handleClear} style={buttonStyle(text === "", true)}>Clear Text</button>
                 </div>
                 <hr />
                 <div className="my-4">
                     <h2>Text Summary</h2>
                     <ul style={{listStyleType: "square"}}>
-                        <li>Words = {text.trim() === ""? 0:text.split(/\s+/).length}</li>
+                        <li>Words = {text.trim() === ""? 0: (text.match(/\b\w+\b/g)) ? text.match(/\b\w+\b/g).length : 0}</li>
                         <li>Characters = {text.length}</li>
                         <li>Read time = {(0.008 * (text.trim() === ""? 0:text.split(" ").length)).toFixed(2)}</li>
                     </ul>
@@ -92,7 +93,7 @@ export default function Content({mode, showAlert}) {
                     <div className="preview" style={myStyle}>
                         <p>{text === "" ? "Nothing to show!":text}</p>
                     </div>
-                    <button className="btn btn-success px-4 my-3 me-5" disabled={text === ""} onClick={handleCopyText} style={buttonStyle(text === "", false)}>Copy Text</button>
+                    <button className="btn btn-success px-4 my-3" disabled={text === ""} onClick={handleCopyText} style={buttonStyle(text === "", false)}>Copy Text</button>
                 </div>
             </div>
         </>
